8b7117b6d99ca58efcf7092a4a5f4660bfd04006
Ch-MANASA19 <manasachimata19@gmail.com>
2025-09-30 19:36:38 +0530

    largenum

---
+++ largestnum.py
@@ -0,0 +1,8 @@
+//Python program to find the largest number among the three input numbers
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
+print("The largest number is", largest)
