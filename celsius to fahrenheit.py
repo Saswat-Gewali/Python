97fdd969e5e973977de71e40a304a47b4145bac9
Ch-MANASA19 <manasachimata19@gmail.com>
2025-09-30 19:52:35 +0530

    Update celsius to fahrenheit

--- celsius to fahrenheit.py
+++ celsius to fahrenheit.py
@@ -3,8 +3,7 @@
 celsius = 37.5
 
 # calculate fahrenheit
-fahrenheit = (celsius * 1.8) + 32
+
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
 
-output:
-37.5 degree Celsius is equal to 99.5 degree Fahrenheit
+
